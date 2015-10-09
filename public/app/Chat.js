583fe3b6c0e84b6a91ab8538c4e09d03a445e922
Malory martin <gueishe@gmail.com>
2015-10-09 19:23:51 +0200

    Amélioration : historique s'étend quand chat caché

--- public/app/Chat.js
+++ public/app/Chat.js
@@ -1,16 +1,19 @@
 var showButtonClick = document.querySelector("#btnShow"),
             hideButtonClick = document.querySelector("#btnHide"),
-            content = document.querySelector("#chat");
-        //Show wrapper div content
+            chat = document.querySelector("#chat");
         showButtonClick.addEventListener("click", function () {
-            if( content.className.indexOf("hidden") !== -1 ) {
-                content.className = content.className.replace(" hidden","");
+            console.log(history);
+            if( chat.className.indexOf("hidden") !== -1 ) {
+                var history = document.querySelector("#history");
+                chat.className = chat.className.replace(" hidden","");
+                history.className = history.className.replace("col-xs-12", "col-xs-9");
             }
         }, false);
 
-        //Hide wrapper div content
         hideButtonClick.addEventListener("click", function () {
-            if( content.className.indexOf("hidden") === -1 ) {
-                content.className += " hidden";
+            if( chat.className.indexOf("hidden") === -1 ) {
+                var history = document.querySelector("#history");
+                chat.className += " hidden";
+                history.className = history.className.replace("col-xs-9", "col-xs-12");
             }
         }, false);

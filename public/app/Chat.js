438e303f00c4a38e05bb6c2f1ea34dc724d67379
Malory martin <gueishe@gmail.com>
2015-10-09 20:39:11 +0200

    Changement du bouton toggle

--- public/app/Chat.js
+++ public/app/Chat.js
@@ -1,19 +1,33 @@
-var showButtonClick = document.querySelector("#btnShow"),
-            hideButtonClick = document.querySelector("#btnHide"),
+var buttonClick = document.querySelector("#btnHide"),
             chat = document.querySelector("#chat");
-        showButtonClick.addEventListener("click", function () {
-            console.log(history);
-            if( chat.className.indexOf("hidden") !== -1 ) {
-                var history = document.querySelector("#history");
-                chat.className = chat.className.replace(" hidden","");
-                history.className = history.className.replace("col-xs-12", "col-xs-9");
-            }
-        }, false);
 
-        hideButtonClick.addEventListener("click", function () {
-            if( chat.className.indexOf("hidden") === -1 ) {
-                var history = document.querySelector("#history");
-                chat.className += " hidden";
-                history.className = history.className.replace("col-xs-9", "col-xs-12");
-            }
-        }, false);
+buttonClick.addEventListener("click", function () {
+    var history = document.querySelector("#history")
+    if( buttonClick.id === "btnHide" ) {
+        hide(buttonClick, chat, history);
+    } else if ( buttonClick.id === "btnShow" ) {
+        show(buttonClick, chat, history);
+    } else {
+        console.log("Erreur dans le toggle du chat.");
+    }
+}, false);
+
+
+function hide(button, content, content2) {
+    if( content.className.indexOf("hidden") === -1 ) {
+        content.className += " hidden";
+        content2.className = content2.className.replace("col-xs-9", "col-xs-12");
+        button.id = "btnShow";
+        console.log(button);
+        button.innerHTML = "Show Chat";
+    }
+}
+
+function show(button, content, content2) {
+    if( content.className.indexOf("hidden") !== -1 ) {
+        content.className = content.className.replace(" hidden","");
+        content2.className = content2.className.replace("col-xs-12", "col-xs-9");
+        button.id = "btnHide";
+        button.innerHTML = "Hide Chat";
+    }
+}
